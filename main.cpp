90254c00d2f0e3d7c9b40791c16e3fa6592bce35
David Kanenwisher <d.kanen@gmail.com>
2014-09-16 21:40:12 -0700

    working on getting the connection working

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <w32api/winsock2.h>
+#include <sys/unistd.h>
 
 using namespace std;
 
@@ -36,16 +38,26 @@ int main() {
         error("Can't open socket");
     }
 
+    struct sockaddr_in name;
+    name.sin_family = PF_INET;
+    name.sin_port = htons(30000);
+    name.sin_addr.s_addr = htonl(INADDR_ANY);
+
     //listen
     if(listen(listener_d, 10) == -1) {
         error("Can't listen");
     }
+    puts("Waiting for connection");
 
-    //accept
-    struct sockaddr_storage client_addr;
-    socklen_t address_size = sizeof(client_addr);
-    int connect_d = accept(listener_d, (struct sockaddr *)&client_addr, &address_size);
-    if(connect_d == -1)
-        error("Can't open secondary socket");
-  return 0;
+    while(1) {
+        //accept
+        struct sockaddr_storage client_addr;
+        int address_size = sizeof(client_addr);
+        int connect_d = accept(listener_d, (struct sockaddr *)&client_addr, &address_size);
+        char *msg = "Why, Hello there?";
+
+        send(connect_d, msg, strlen(msg), 0);
+        close(connect_d);
+    }
+    return 0;
 }
